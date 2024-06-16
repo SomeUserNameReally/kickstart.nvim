58b1071741a12e4f85cde5cd4403794580fc454f
Ajay Singh <ajayhasanemail@gmail.com>
2024-06-16 12:52:10 -0400

    Add plugins and adjust init

--- init.lua
+++ init.lua
@@ -161,8 +161,8 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
-        theme = 'onedark',
+        icons_enabled = true,
+        theme = 'gruvbox',
         component_separators = '|',
         section_separators = '',
       },
@@ -228,7 +228,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,16 +1,21 @@
--- You can add your own plugins here or in other files in this directory!
+-- You can add your own  ins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
 return {
   -- gruvbox theme
-  {'morhetz/gruvbox', config = function() vim.cmd.colorscheme('gruvbox') end },
+  {'morhetz/gruvbox', name='gruvbox'},
   -- Noctis Theme
-  { 'talha-akram/noctis.nvim', name = 'noctis' },
+  -- config = function() vim.cmd.colorscheme('gruvbox') end
+  { 'talha-akram/noctis.nvim', name = 'noctis', config = function() vim.cmd.colorscheme('noctis_lux') end },
   -- Catppuccin theme
   { 'catppuccin/nvim', name = 'catppuccin' },
 
   -- nvim-tree
   'nvim-tree/nvim-tree.lua',
   'nvim-tree/nvim-web-devicons',
+  'dart-lang/dart-vim-plugin',  'thosakwe/vim-flutter',  'natebosch/vim-lsc',  'natebosch/vim-lsc-dart',
+  {'akinsho/flutter-tools.nvim', dependencies = {'nvim-lua/plenary.nvim', 'stevearc/dressing.nvim',}, config = true},
+  { 'Exafunction/codeium.vim', event = 'BufEnter'},
+  'github/copilot.vim'
 }
