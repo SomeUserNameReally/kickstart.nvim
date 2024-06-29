aeb5030282a843a7b97bcf592c343013ec738838
Ajay Singh <ajayhasanemail@gmail.com>
2024-06-29 04:35:42 -0400

    Remove github copilot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,5 +17,4 @@ return {
   'dart-lang/dart-vim-plugin',  'thosakwe/vim-flutter',  'natebosch/vim-lsc',  'natebosch/vim-lsc-dart',
   {'akinsho/flutter-tools.nvim', dependencies = {'nvim-lua/plenary.nvim', 'stevearc/dressing.nvim',}, config = true},
   { 'Exafunction/codeium.vim', event = 'BufEnter'},
-  'github/copilot.vim'
 }
